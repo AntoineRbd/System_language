a6678c288d04f1df9323f0ed12a4851fd5d5de59
AntoineR <antoine.roubaud@epita.frgit config --global user.email antoine.roubaud@epita.fr>
2022-11-01 11:52:27 +0100

    Continue exe8

--- rust/exe8/src/main.rs
+++ rust/exe8/src/main.rs
@@ -10,23 +10,42 @@ enum Expr {
     Literal(i32)
 }
 
-impl Expr {
-    fn eval(&self) -> i32 {
-        return match self {
-            Expr::Literal(a) => *a,
-            //Expr::IfExpr { cond, true_branch, false_branch } => eval(*IfExpr(cond, true_branch, false_branch)),
-            Expr::BinOp { l, op, r } => self.eval(self.BinOp(l, op, r, self)),
-            _ => 0 as i32
-        };
-    }
+fn eval(e: Expr) -> i32 {
+    return match e {
+        Expr::Literal(a) => a,
+        Expr::IfExpr { cond, true_branch, false_branch } => eval(*IfExpr(cond, true_branch, false_branch)),
+        Expr::BinOp { l, op, r } => eval(*BinOp(l, op, r)),
+        _ => 0 as i32
+    };
+}
 
-    fn BinOp(&self, l: Box<Expr>, op: Operator, r: box<Expr>) -> Expr::Literal {
-        return match op {
-            Operator::Plus => self.eval(l) + self.eval(r),
-            Operator::Minus => self.eval(l) - self.eval(r),
-            Operator::Divide => self.eval(l) / self.eval(r),
-            Operator::Multiply=> self.eval(l) * self.eval(r),
-        };
+fn BinOp(l: Box<Expr>, op: Operator, r: Box<Expr>) -> Box<Expr> {
+    let intR = eval(*r);
+    let intL = eval(*l);
+
+    return match op {
+        Operator::Plus => Box::new(Expr::Literal(intL + intR)),
+        Operator::Minus => Box::new(Expr::Literal(intL - intR)),
+        Operator::Multiply=> Box::new(Expr::Literal(intL * intR)),
+        Operator::Divide => {
+            if intR != 0 {
+                return Box::new(Expr::Literal(intL / intR));
+            }
+            else {
+                return Box::new(Expr::Literal(-1));
+            }
+        },
+        _ => Box::new(Expr::Literal(-1))
+
+    };
+}
+
+fn IfExpr(cond: Box<Expr>, true_branch: Box<Expr>, false_branch: Box<Expr>) -> Box<Expr> {
+    if eval(*cond) != 0 {
+        return true_branch;
+    }
+    else {
+        return false_branch;
     }
 }
 
@@ -39,5 +58,58 @@ fn test_plus() {
     let l2 = Expr::Literal(2);
 
     let add = Expr::BinOp { l: Box::new(l1), op: Operator::Plus, r: Box::new(l2) };
-    assert_eq!(add.eval(), 10 + 2);
+    assert_eq!(eval(add), 10 + 2);
+}
+
+#[test]
+fn test_minus() {
+    let l1 = Expr::Literal(10);
+    let l2 = Expr::Literal(2);
+
+    let min = Expr::BinOp { l: Box::new(l1), op: Operator::Minus, r: Box::new(l2) };
+    assert_eq!(eval(min), 10 - 2);
+}
+
+#[test]
+fn test_multiply() {
+    let l1 = Expr::Literal(10);
+    let l2 = Expr::Literal(2);
+
+    let mult = Expr::BinOp{l: Box::new(l1), op: Operator::Multiply, r: Box::new(l2)};
+    assert_eq!(eval(mult), 10 * 2);
+}
+
+#[test]
+fn test_multiply_by_0() {
+    let l1 = Expr::Literal(10);
+    let l2 = Expr::Literal(0);
+
+    let mult = Expr::BinOp{l: Box::new(l1), op: Operator::Multiply, r: Box::new(l2)};
+    assert_eq!(eval(mult), 10 * 0);
+}
+
+#[test]
+fn test_divide() {
+    let l1 = Expr::Literal(10);
+    let l2 = Expr::Literal(2);
+
+    let div = Expr::BinOp{l: Box::new(l1), op: Operator::Divide, r: Box::new(l2)};
+    assert_eq!(eval(div), 10 / 2);
+}
+
+#[test]
+fn test_divide_by_0() {
+    let l1 = Expr::Literal(10);
+    let l2 = Expr::Literal(0);
+
+    let div = Expr::BinOp{l: Box::new(l1), op: Operator::Divide, r: Box::new(l2)};
+    assert_eq!(eval(div), -1);
+}
+
+#[test]
+fn test_if_expr_true() {
+    let l1 = Expr::Literal(10);
+
+    let test = Expr::IfExpr{cond: Box::new(l1), true_branch: Box::new(1), false_branch: Box::new(0)};
+    assert_eq!(eval(test), );
 }
