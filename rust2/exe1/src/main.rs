b89d8b9bc2ee0eabd52f6e3147891f663e6cee7d
AntoineR <antoine.roubaud@epita.frgit config --global user.email antoine.roubaud@epita.fr>
2022-11-05 11:59:12 +0100

    Add exe1 TP2 rust, maybe change Vec for supported all types ?

--- rust2/exe1/src/main.rs
+++ rust2/exe1/src/main.rs
@@ -1,39 +1,40 @@
-pub struct SortedList<K: Ord, V: PartialEq> {
-    list: Vec<(K, V)>,
-    index: usize,
+pub struct SortedList {
+    list: Vec<i32>,
 }
 
-impl<K: Ord, V: PartialEq> SortedList<K, V> {
+impl SortedList {
     fn new() -> Self {
         SortedList {
             list: Vec::new(),
-            index: 0,
         }
     }
 
-    fn insert(&mut self, key: K, value: V) {
-        self.list.push((key, value));
-        //self.list.sort_by_key(|k| k.abs());
+    fn insert(&mut self, value: i32) {
+        self.list.push(value);
+        self.list.sort();
     }
 
-    fn iter(&mut self) -> (K, V) {
-        let i = self.list[self.index];
-        self.index += 1;
-        return i;
-    }
-
-
+    /*
     fn print(&self) {
-        //println!("List: {:#?}", self.list);
+        println!("list: {:?}", self.list);
     }
+    */
 }
 
 
 fn main() {
-    let mut list: SortedList<i32, i32> = SortedList::new();
-    list.insert(1, 1);
-    list.insert(0, 3);
-    list.insert(2, 12);
+}
 
-    assert_eq!(list.iter().collect::<Vec<_>>(), vec![(&1, 1), (&0, 3), (&2, 12)]);
+#[test]
+fn test_insertion() {
+    let mut list: SortedList = SortedList::new();
+
+    list.insert(1);
+    list.insert(0);
+    list.insert(12);
+    list.insert(42);
+    list.insert(-12);
+    list.insert(7);
+
+    assert_eq!(list.list, vec![-12, 0, 1, 7, 12, 42]);
 }
